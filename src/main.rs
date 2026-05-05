1f97daab41d0b534dd020b65ba892adaabdd1309
Franco Dalla Via Oliveros <francodvo@gmail.com>
2026-05-05 18:10:58 -0300

    fresh start

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,21 @@
 use std::{env::args, path::Path};
 
-use crate::{arena::Arena, checker::Checker, error::Error, resolver::Resolver};
+use crate::{arena::Arena, error::Error};
 
 mod arena;
-mod checker;
 mod error;
 mod lexer;
-mod parser;
-mod resolver;
-mod structure;
 
 fn main() -> Result<(), Error> {
     let args = args().collect::<Vec<String>>();
     if args.len() < 2 {
-        return Err(Error::Custom("Usage: erwin <file>".to_string()));
+        return Err(Error::Custom("Usage: erw <file>".to_string()));
     }
 
     let std_path = Path::new("/usr/lib/erwin/std/").canonicalize()?;
     let prelude_module = std_path.join("prelude.erw").canonicalize()?;
     let main_module = Path::new(&args[1]).canonicalize()?;
     let arena = Box::leak(Box::new(Arena::new()));
-
-    let registry = Resolver::new(&arena, &std_path, &main_module).resolve()?;
-
-    let mut checker = Checker::new(&arena, &std_path, &prelude_module, &main_module, &registry);
-    let (instructions, literals) = checker.check()?;
-    checker.debug()?;
 
     Ok(())
 }
